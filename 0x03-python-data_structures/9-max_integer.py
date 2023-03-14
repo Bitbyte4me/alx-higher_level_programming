217facd4454d5d2d12861caee3b2540f995a21c9
Olawale <horlah2mee@gmail.com>
2023-03-14 10:50:12 -0700

    the max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if len(my_list) < 1:
         return None
-    biggest = 0
-    for index in my_list:
-        if index > biggest:
-            biggest = index
-    return biggest
+    list_copy = my_list.copy()
+    list_copy.sort()
+    return list_copy[-1]
